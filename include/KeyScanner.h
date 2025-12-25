587e0df4f8b950e7dda640d134219637c5bb1f25
y-decimal <83334510+y-decimal@users.noreply.github.com>
2025-12-26 00:03:14 +0100

    refactor: remove hardcoded pin definitions and make getBitIndex parameter more flexible

--- include/KeyScanner.h
+++ include/KeyScanner.h
@@ -3,19 +3,6 @@
 
 #include <Arduino.h>
 
-// For now we will assume the pins and wiring layout are always the same for
-// simplicity, may be made more modular later
-
-#define ROW_1_PIN 9
-#define ROW_2_PIN 10
-#define COL_1_PIN 17
-#define COL_2_PIN 18
-#define ROWS 2
-#define COLS 2
-
-static constexpr int rowPins[ROWS] = {ROW_1_PIN, ROW_2_PIN};
-static constexpr int colPins[COLS] = {COL_1_PIN, COL_2_PIN};
-
 class KeyScanner {
 private:
   uint8_t *keyState;
@@ -25,7 +12,7 @@ private:
   uint8_t *colPins;
   void setKey(uint8_t *row, uint8_t *col);
   void clearKey(uint8_t *row, uint8_t *col);
-  const uint8_t getBitIndex(uint8_t *row, uint8_t *col, uint8_t cols = COLS);
+  const uint8_t getBitIndex(uint8_t *row, uint8_t *col, uint8_t *cols);
 
 #ifdef UNIT_TEST
   friend class TestKeyScanner;
