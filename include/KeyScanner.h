828a1d201b3d90abeea74226aba5392eb21ead66
y-decimal <83334510+y-decimal@users.noreply.github.com>
2025-12-25 23:29:37 +0100

    feat: refactor KeyHandler class to KeyScanner and outline class structure

--- include/KeyScanner.h
+++ include/KeyScanner.h
@@ -1,13 +1,34 @@
-#ifndef KEYHANDLER_H
-#define KEYHANDLER_H
+#ifndef KEYSCANNER_H
+#define KEYSCANNER_H
 
-#include <SharedTypes.h>
+#include <Arduino.h>
 
-class KeyHandler {
+// For now we will assume the pins and wiring layout are always the same for
+// simplicity, may be made more modular later
+
+#define ROW_1_PIN 9
+#define ROW_2_PIN 10
+#define COL_1_PIN 17
+#define COL_2_PIN 18
+#define ROWS 2
+#define COLS 2
+
+static constexpr int rowPins[ROWS] = {ROW_1_PIN, ROW_2_PIN};
+static constexpr int colPins[COLS] = {COL_1_PIN, COL_2_PIN};
+
+class KeyScanner {
 private:
+  uint8_t keyState[5] = {0};
+
+  void updateKeyState();
+  void setKey(uint8_t *row, uint8_t *col);
+  void clearKey(uint8_t *row, uint8_t *col);
+  const uint8_t getBitIndex(uint8_t *row, uint8_t *col, uint8_t cols = COLS);
 
 public:
-
+  KeyScanner();
+  const uint8_t *getKeyState();
+  const bool getKey(uint8_t *bitMap, uint8_t row, uint8_t col, uint8_t cols);
 };
 
 #endif
