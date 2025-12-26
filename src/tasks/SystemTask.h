6cc678c75c251fe325309fabf35a2c94236c16f2
y-decimal <83334510+y-decimal@users.noreply.github.com>
2025-12-26 22:31:44 +0100

    feat: add SystemTask to initialize KeyScanner and manage key events

---
+++ src/tasks/SystemTask.h
@@ -0,0 +1,14 @@
+#include <FreeRTOS.h>
+#include <system/SharedTypes.h>
+#include <tasks/KeyScannerTask.h>
+
+QueueHandle_t keyEventQueue;
+
+extern KeyScanner keyScanner;
+
+void initSystemTasks() {
+  // Initialize KeyScanner Task
+  keyEventQueue = xQueueCreate(32, sizeof(KeyEvent));
+  xTaskCreatePinnedToCore(keyScannerTask, "KeyScanner", STACK_KEYSCAN,
+                          &keyScanner, PRIORITY_KEYSCAN, nullptr, CORE_KEYSCAN);
+}
