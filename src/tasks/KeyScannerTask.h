1b1003e9641bbb26b8e9c88a04e4c07d662d2f91
y-decimal <83334510+y-decimal@users.noreply.github.com>
2025-12-26 22:31:25 +0100

    feat: implement keyEventCallback

--- src/tasks/KeyScannerTask.h
+++ src/tasks/KeyScannerTask.h
@@ -1,8 +1,18 @@
 #include <modules/KeyScanner.h>
+#include <system/SharedTypes.h>
 #include <system/SystemConfig.h>
+
+extern QueueHandle_t keyEventQueue;
+
+void keyEventCallback(uint16_t keyIndex, bool state) {
+  KeyEvent event{keyIndex, state};
+  xQueueSend(keyEventQueue, &event, 0);
+}
 
 void keyScannerTask(void *arg) {
   KeyScanner *keyScanner = static_cast<KeyScanner *>(arg);
+
+  keyScanner->registerOnKeyChangeCallback(keyEventCallback);
 
   TickType_t previousWakeTime = xTaskGetTickCount();
 
