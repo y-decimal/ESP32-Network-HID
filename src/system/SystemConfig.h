7a192d3f460b4073414a6ed4921b2593cafd15db
y-decimal <83334510+y-decimal@users.noreply.github.com>
2025-12-26 22:40:09 +0100

    feat: add KeyScanner pin definitions

--- src/system/SystemConfig.h
+++ src/system/SystemConfig.h
@@ -5,3 +5,7 @@ static const uint32_t STACK_KEYSCAN = 4096;
 static const UBaseType_t PRIORITY_KEYSCAN = 5;
 static const BaseType_t CORE_KEYSCAN = 1;
 static const TickType_t PERIOD_KEYSCAN_MS = 1;
+
+// KeyScanner Pin Definitions
+static const uint8_t ROWPINS[2] = {9, 10};
+static const uint8_t COLPINS[2] = {17, 18};
