aa6512837f2a7502a01ef3d4755264fe5dab85df
y-decimal <83334510+y-decimal@users.noreply.github.com>
2025-12-27 23:20:40 +0100

    fix: rename DeviceID enum to DeviceRole for clarity

--- src/shared/CommTypes.h
+++ src/shared/CommTypes.h
@@ -5,6 +5,7 @@
 
 enum class PacketType : uint8_t { KeyDataHalf, KeyDataFull, Count };
 
-enum class DeviceID : uint8_t { KeyboardLeft, KeyboardRight, Dongle, Count };
+enum class DeviceRole : uint8_t { KeyboardLeft, KeyboardRight, Dongle, Count };
+
 
 #endif
