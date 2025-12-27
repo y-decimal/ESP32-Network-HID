85e17874409f6d97cfd25e3ca04fc525fdd96a47
y-decimal <83334510+y-decimal@users.noreply.github.com>
2025-12-27 22:21:48 +0100

    fix: add missing include guards at end of files in KeyTypes.h and CommTypes.h

--- src/shared/CommTypes.h
+++ src/shared/CommTypes.h
@@ -1,7 +1,10 @@
+#ifndef COMMTYPES_H
+#define COMMTYPES_H
+
 #include <stdint.h>
 
 enum class PacketType : uint8_t { KeyDataHalf, KeyDataFull, Count };
 
 enum class DeviceID : uint8_t { KeyboardLeft, KeyboardRight, Dongle, Count };
 
-
+#endif
--- src/shared/KeyTypes.h
+++ src/shared/KeyTypes.h
@@ -1,6 +1,11 @@
+#ifndef KEYTYPES_H
+#define KEYTYPES_H
+
 #include <stdint.h>
 
 struct KeyEvent {
   uint16_t keyIndex;
   bool state;
 };
+
+#endif
