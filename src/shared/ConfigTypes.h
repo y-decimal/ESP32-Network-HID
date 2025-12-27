629a17ef6989ce8f1139d7d1e1fbcf4b23088989
y-decimal <83334510+y-decimal@users.noreply.github.com>
2025-12-27 23:12:13 +0100

    feat: add HardwareConfig and UserConfig structures for configuration management

---
+++ src/shared/ConfigTypes.h
@@ -0,0 +1,35 @@
+#ifndef CONFIGTYPES_G
+#define CONFIGTYPES_H
+
+#include <shared/CommTypes.h>
+#include <stdint.h>
+
+using pinType = uint8_t;
+using countType = uint8_t;
+
+static constexpr uint8_t MAX_PIN_COUNT = UINT8_MAX;
+
+struct HardwareConfig {
+
+  struct KeyMatrixConfig {
+    countType rows;
+    countType cols;
+    pinType rowPins[MAX_PIN_COUNT];
+    pinType colPins[MAX_PIN_COUNT];
+  };
+
+  struct InternalPinConfig {
+    pinType internalLedPin;
+    pinType internalRgbPin;
+  };
+
+  //
+
+  DeviceID selfID;
+  KeyMatrixConfig keyMatrix;
+  InternalPinConfig internalPin;
+};
+
+struct UserConfig {};
+
+#endif
