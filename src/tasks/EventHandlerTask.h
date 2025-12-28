f793821cdccda4418bd7ea06e6a1ce3a9a37e3ea
y-decimal <83334510+y-decimal@users.noreply.github.com>
2025-12-28 16:16:46 +0100

    feat: add EventTask for handling events in FreeRTOS

---
+++ src/tasks/EventHandlerTask.h
@@ -0,0 +1,16 @@
+#include <FreeRTOS.h>
+#include <modules/EventRegistry.h>
+#include <queue.h>
+
+void EventTask(void *arg) {
+  QueueHandle_t queue = static_cast<QueueHandle_t>(arg);
+  Event event;
+
+  while (true) {
+    if (xQueueReceive(queue, &event, portMAX_DELAY)) {
+      auto handler = EventRegistry::getHandler(event.type);
+      if (handler)
+        handler(event);
+    }
+  }
+}
