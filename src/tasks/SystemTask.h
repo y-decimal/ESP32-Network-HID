10c21631baf48efb1aff9155f168cb5b429f904e
y-decimal <83334510+y-decimal@users.noreply.github.com>
2025-12-28 16:29:02 +0100

    feat: update SystemTask to create priority and event queues for event handling feat: initialize priority event queue handler task in initSystemTasks

--- src/tasks/SystemTask.h
+++ src/tasks/SystemTask.h
@@ -2,16 +2,23 @@
 #define SYSTEMTASK_H
 
 #include <FreeRTOS.h>
-#include <shared/EventTypes.h>
+#include <tasks/EventHandlerTask.h>
 #include <tasks/KeyScannerTask.h>
 
-extern QueueHandle_t EventQueue;
+extern QueueHandle_t priorityEventQueue;
+extern QueueHandle_t eventQueue;
 
 extern KeyScanner keyScanner;
 
 void initSystemTasks() {
   // Initialize KeyScanner Task
-  EventQueue = xQueueCreate(32, sizeof(KeyEvent));
+  priorityEventQueue = xQueueCreate(32, sizeof(Event));
+  eventQueue = xQueueCreate(32, sizeof(Event));
+
+  xTaskCreatePinnedToCore(EventTask, "PriorityEventHandler",
+                          STACK_PRIORITYEVENT, &priorityEventQueue,
+                          PRIORITY_PRIORITYEVENT, nullptr, CORE_PRIORITYEVENT);
+
   xTaskCreatePinnedToCore(keyScannerTask, "KeyScanner", STACK_KEYSCAN,
                           &keyScanner, PRIORITY_KEYSCAN, nullptr, CORE_KEYSCAN);
 }
