77016b956b423a334fc2ac3fa4ee7f45e77ee976
y-decimal <83334510+y-decimal@users.noreply.github.com>
2025-12-28 21:30:38 +0100

    refactor: rename DataTypes.h to ISerializableStruct.h and move it to interfaces

--- src/interfaces/ISerializableStructs.h
+++ src/interfaces/ISerializableStructs.h
@@ -1,5 +1,5 @@
-#ifndef DATATYPES_H
-#define DATATYPES_H
+#ifndef ISERIALIZABLESTRUCT_H
+#define ISERIALIZABLESTRUCT_H
 
 #include <cstring>
 #include <stdint.h>
@@ -7,15 +7,6 @@
 struct Serializable {
   virtual size_t pack(uint8_t *output) const = 0;
   virtual size_t unpack(const uint8_t *input) = 0;
-};
-
-struct LocalBitMap : Serializable {
-  const uint8_t *bitMapPtr;
-  uint16_t size;
-
-  size_t pack(uint8_t *output) const {
-
-  }
 };
 
 #endif
