f67ac53d978044cb4d3036548d811ba2e384c597
y-decimal <83334510+y-decimal@users.noreply.github.com>
2025-12-28 20:02:03 +0100

    feat: refactor simulateConfig to encapsulate key configuration setup fix: update initSystemTasks call to pass mainCfg style: adjust includes and remove unused variables

--- src/main.cpp
+++ src/main.cpp
@@ -1,15 +1,14 @@
 #include <Arduino.h>
-#include <tasks/SystemTask.h>
+#include <modules/SystemTask.h>
 
 // temp local definitions for testing
-uint8_t ROWPINS[2] = {9, 10};
-uint8_t COLPINS[2] = {17, 18};
-uint8_t rowCount = 2;
-uint8_t colCount = 2;
 
+ConfigManager mainCfg;
+
+// Define global variables from SystemTask.h
 QueueHandle_t priorityEventQueue;
 QueueHandle_t eventQueue;
-KeyScanner keyScanner = KeyScanner(ROWPINS, COLPINS, rowCount, colCount);
+KeyScannerConfig keyCfg;
 
 void keyPrintCallback(const Event &event) {
   if (event.type != EventType::Key) {
@@ -22,14 +21,38 @@ void keyPrintCallback(const Event &event) {
   printf("Key event: Key %d %s\n", keyIndex, state ? "pressed" : "released");
 }
 
+void simulateConfig() {
+  DeviceRole roles[1] = {DeviceRole::Keyboard};
+  MacAddress mac = {0xAA, 0xBB, 0xCC, 0xDD, 0xEE, 0xFF};
+  GlobalConfig gCfg;
+  memcpy(gCfg.roles, roles, 1);
+  memcpy(gCfg.deviceMac, mac, 6);
+  mainCfg.setGlobalConfig(gCfg);
+
+  uint8_t ROWPINS[2] = {9, 10};
+  uint8_t COLPINS[2] = {17, 18};
+  uint8_t rowCount = 2;
+  uint8_t colCount = 2;
+  uint16_t refreshRate = 1000;
+  KeyScannerConfig kCfg;
+  memcpy(kCfg.rowPins, ROWPINS, 2);
+  memcpy(kCfg.colPins, COLPINS, 2);
+  kCfg.rows = rowCount;
+  kCfg.cols = colCount;
+  kCfg.refreshRate = 1000;
+
+  mainCfg.setGlobalConfig(gCfg);
+  mainCfg.setKeyConfig(kCfg);
+}
+
 void setup() {
   Serial.begin(115200);
   delay(3000);
   printf("initializing...\n");
+  simulateConfig();
   EventRegistry::registerHandler(EventType::Key, keyPrintCallback);
-  initSystemTasks();
+  initSystemTasks(&mainCfg);
   printf("setup done\n");
 }
 
-void loop() {
-}
+void loop() {}
