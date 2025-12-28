10fa6dcfe4f62cea42dfc459978ad85b3c1aa56c
y-decimal <83334510+y-decimal@users.noreply.github.com>
2025-12-28 23:27:45 +0100

    feat: add BitMapSenderTask to make a local copy of the shared bitMapState at fixed intervals and later call a callback handed through the Parameters struct

---
+++ src/modules/BitMapSenderTask.h
@@ -0,0 +1,34 @@
+#ifndef BITMAPSENDERTASK_H
+#define BITMAPSENDERTASK_H
+
+#include <FreeRTOS.h>
+#include <modules/TaskParameters.h>
+#include <queue.h>
+#include <shared/ConfigTypes.h>
+#include <shared/DataTypes.h>
+#include <task.h>
+
+extern uint8_t *bitMapBuffer;
+
+void bitMapSenderTask(void *arg) {
+  BitMapSenderParameters *params = static_cast<BitMapSenderParameters *>(arg);
+
+  BitMapSenderConfig *moduleCfg = params->config;
+  KeyScannerState *state = params->state;
+
+  uint8_t bitMapSize = state->bitMapSize;
+  uint16_t refreshRate = moduleCfg->refreshRate;
+
+  uint8_t localBitmapCopy[bitMapSize];
+
+  TickType_t refreshRateTicks = pdMS_TO_TICKS(refreshRate / 1000);
+
+  TickType_t previousWakeTime = xTaskGetTickCount();
+
+  while (true) {
+    memcpy(localBitmapCopy, state->bitMap, state->bitMapSize);
+    xTaskDelayUntil(&previousWakeTime, refreshRateTicks);
+  }
+}
+
+#endif
