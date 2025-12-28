ade0ce5aeba98d0f007fb1b78f8e06076e392fc6
y-decimal <83334510+y-decimal@users.noreply.github.com>
2025-12-28 22:05:19 +0100

    feat: implement Serializable inheritance for GlobalConfig and KeyScannerConfig structs feat: begin adding pack function to KeyScannerConfig for data serialization

--- src/shared/ConfigTypes.h
+++ src/shared/ConfigTypes.h
@@ -2,6 +2,7 @@
 #define CONFIGTYPES_H
 
 #include <cstring>
+#include <interfaces/ISerializableStructs.h>
 #include <shared/CommTypes.h>
 #include <stdint.h>
 
@@ -10,7 +11,7 @@ using countType = uint8_t;
 
 static constexpr uint8_t MAX_PIN_COUNT = 50;
 
-struct GlobalConfig {
+struct GlobalConfig : Serializable {
   DeviceRole roles[(size_t)DeviceRole::Count]{};
   MacAddress deviceMac{};
 
@@ -22,7 +23,7 @@ struct GlobalConfig {
   void setMac(MacAddress mac) { memcpy(deviceMac, mac, 6); }
 };
 
-struct KeyScannerConfig {
+struct KeyScannerConfig : Serializable {
   countType rows = 0;
   countType cols = 0;
   pinType rowPins[MAX_PIN_COUNT]{};
@@ -38,6 +39,31 @@ struct KeyScannerConfig {
     if (arrSize > MAX_PIN_COUNT)
       return;
     memcpy(colPins, colPinArray, arrSize);
+  }
+
+  size_t pack(uint8_t *output) {
+
+    size_t actualRowPinBytes = 0;
+    for (size_t i; i < MAX_PIN_COUNT; i++) {
+      if (rowPins[i] != 0)
+        actualRowPinBytes++;
+    }
+
+    size_t actualColPinBytes = 0;
+    for (size_t i; i < MAX_PIN_COUNT; i++) {
+      if (colPins[i] != 0)
+        actualColPinBytes++;
+    }
+
+    constexpr uint8_t bitSpacePinArray = MAX_PIN_COUNT / 8;
+
+    size_t byteSize = 2 * bitSpacePinArray + actualRowPinBytes +
+                      actualColPinBytes + sizeof(rows) + sizeof(cols) +
+                      sizeof(refreshRate);
+
+    uint8_t buffer[byteSize];
+
+    // Finish implementation later
   }
 };
 
