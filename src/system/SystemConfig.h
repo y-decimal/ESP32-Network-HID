e8f1b5c6d0d917ee66b807c78d6eb1308181473a
y-decimal <83334510+y-decimal@users.noreply.github.com>
2025-12-28 19:14:14 +0100

    fix: remove unused PERIOD_KEYSCAN_MS constant

--- src/system/SystemConfig.h
+++ src/system/SystemConfig.h
@@ -10,6 +10,5 @@ static const BaseType_t CORE_PRIORITYEVENT = 1;
 static const uint32_t STACK_KEYSCAN = 4096;
 static const UBaseType_t PRIORITY_KEYSCAN = 5;
 static const BaseType_t CORE_KEYSCAN = 1;
-static const TickType_t PERIOD_KEYSCAN_MS = 1;
 
 #endif
