d3b0055ca4405f89f7063486be9b8e8b19a9993f
y-decimal <83334510+y-decimal@users.noreply.github.com>
2025-12-28 15:58:54 +0100

    feat: add generalized Even using unions to allow event agnostic callback registration later feat: add enum to identify packet type for callback mapping

--- src/shared/EventTypes.h
+++ src/shared/EventTypes.h
@@ -3,9 +3,18 @@
 
 #include <stdint.h>
 
+enum class EventType : uint8_t { Key, ConfigUpdate, COUNT };
+
 struct KeyEvent {
   uint16_t keyIndex;
   bool state;
+};
+
+struct Event {
+  EventType type;
+  union {
+    KeyEvent key;
+  };
 };
 
 #endif
