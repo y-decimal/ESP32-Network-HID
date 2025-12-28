1d44b366bafc4d5045d424b06ec517b7ea899bc6
y-decimal <83334510+y-decimal@users.noreply.github.com>
2025-12-28 20:33:07 +0100

    refactor: update get and set methods for GlobalConfig and KeyScannerConfig to use encapsulation feat: implement saveConfig and loadConfig methods for better configuration management

--- src/submodules/ConfigManager.cpp
+++ src/submodules/ConfigManager.cpp
@@ -1,9 +1,39 @@
 #include <submodules/ConfigManager.h>
 
-GlobalConfig ConfigManager::getGlobalConfig() const { return globalCfg; }
+GlobalConfig ConfigManager::getGlobalConfig() const { return globalCfg.get(); }
 
-KeyScannerConfig ConfigManager::getKeyConfig() const { return keyScannerCfg; }
+KeyScannerConfig ConfigManager::getKeyConfig() const {
+  return keyScannerCfg.get();
+}
 
-void ConfigManager::setGlobalConfig(GlobalConfig cfg) { globalCfg = cfg; }
+void ConfigManager::setGlobalConfig(GlobalConfig cfg) { globalCfg.set(cfg); }
 
-void ConfigManager::setKeyConfig(KeyScannerConfig cfg) { keyScannerCfg = cfg; }
+void ConfigManager::setKeyConfig(KeyScannerConfig cfg) {
+  keyScannerCfg.set(cfg);
+}
+
+bool ConfigManager::saveConfig() {
+
+  bool globalSaved = false;
+  bool keySaved = false;
+
+  if (globalCfg.isDirty())
+    globalSaved = globalCfg.save();
+  if (keyScannerCfg.isDirty())
+    keySaved = keyScannerCfg.save();
+
+  return globalSaved && keySaved;
+}
+
+bool ConfigManager::loadConfig() {
+
+  bool globalLoaded = false;
+  bool keyLoaded = false;
+
+  if (!globalCfg.isDirty())
+    globalLoaded = globalCfg.load();
+  if (!keyScannerCfg.isDirty())
+    keyLoaded = keyScannerCfg.load();
+
+  return globalLoaded && keyLoaded;
+}
