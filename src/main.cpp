e4fabf18a9020ece737f3fdf44ec785a5e169902
y-decimal <83334510+y-decimal@users.noreply.github.com>
2025-12-28 17:06:34 +0100

    refactor: rename EventQueue to priorityEventQueue for clarity refactor: reorganize keyScanner initialization with row and column counts

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,15 @@
 #include <Arduino.h>
 #include <tasks/SystemTask.h>
 
-QueueHandle_t EventQueue;
-KeyScanner keyScanner = KeyScanner(ROWPINS, COLPINS);
+// temp local definitions for testing
+uint8_t ROWPINS[2] = {9, 10};
+uint8_t COLPINS[2] = {17, 18};
+uint8_t rowCount = 2;
+uint8_t colCount = 2;
+
+QueueHandle_t priorityEventQueue;
+QueueHandle_t eventQueue;
+KeyScanner keyScanner = KeyScanner(ROWPINS, COLPINS, rowCount, colCount);
 
 void setup() {
   Serial.begin(115200);
@@ -11,7 +18,7 @@ void setup() {
 
 void loop() {
   KeyEvent event;
-  if (xQueueReceive(EventQueue, &event, pdMS_TO_TICKS(100)) == pdPASS) {
+  if (xQueueReceive(priorityEventQueue, &event, pdMS_TO_TICKS(100)) == pdPASS) {
     printf("Key %d %s\n", event.keyIndex,
            (event.state ? "pressed" : "released"));
   }
