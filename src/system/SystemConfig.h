4cfa434071d46b106365db317488f3c3ca1cefd8
y-decimal <83334510+y-decimal@users.noreply.github.com>
2025-12-28 23:28:48 +0100

    feat: add BitMapSender Task configuration parameters

--- src/system/SystemConfig.h
+++ src/system/SystemConfig.h
@@ -1,6 +1,9 @@
 #ifndef SYSTEMCONFIG_H
 #define SYSTEMCONFIG_H
 
+#include <stdint.h>
+#include <task.h>
+
 // Priority Event Task Config
 static const uint32_t STACK_PRIORITYEVENT = 4096;
 static const UBaseType_t PRIORITY_PRIORITYEVENT = 4;
@@ -10,5 +13,10 @@ static const BaseType_t CORE_PRIORITYEVENT = 1;
 static const uint32_t STACK_KEYSCAN = 4096;
 static const UBaseType_t PRIORITY_KEYSCAN = 5;
 static const BaseType_t CORE_KEYSCAN = 1;
+
+// BitMapSender Task Config
+static const uint32_t STACK_BITMAP = 4096;
+static const UBaseType_t PRIORITY_BITMAP = 3;
+static const BaseType_t CORE_BITMAP = 0;
 
 #endif
