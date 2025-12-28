35f000613b6f54a49d43bb205a9696c46ca6978a
y-decimal <83334510+y-decimal@users.noreply.github.com>
2025-12-28 23:05:43 +0100

    feat: update initSystemTasks to use KeyScannerParameters for key scanner task configuration refactor: reorganize bitmap initialization logic for clarity

--- src/modules/SystemTask.h
+++ src/modules/SystemTask.h
@@ -4,6 +4,7 @@
 #include <FreeRTOS.h>
 #include <modules/EventHandlerTask.h>
 #include <modules/KeyScannerTask.h>
+#include <modules/TaskParameters.h>
 #include <submodules/ConfigManager.h>
 
 extern QueueHandle_t priorityEventQueue;
@@ -12,17 +13,27 @@ extern QueueHandle_t eventQueue;
 extern KeyScannerConfig keyCfg;
 
 void initSystemTasks(ConfigManager *cfgManager) {
+
   priorityEventQueue = xQueueCreate(32, sizeof(Event));
   eventQueue = xQueueCreate(32, sizeof(Event));
 
   keyCfg = cfgManager->getKeyConfig();
 
+  KeyScannerState *scannerState = new KeyScannerState;
+  scannerState->bitMapSize = (keyCfg.rows * keyCfg.cols + 7) / 8;
+  scannerState->bitMap = new uint8_t[scannerState->bitMapSize];
+  memset(scannerState->bitMap, 0, scannerState->bitMapSize);
+
+  KeyScannerParameters *keyParams = new KeyScannerParameters;
+  keyParams->config = &keyCfg;
+  keyParams->state = scannerState;
+
   xTaskCreatePinnedToCore(EventTask, "PriorityEventHandler",
                           STACK_PRIORITYEVENT, priorityEventQueue,
                           PRIORITY_PRIORITYEVENT, nullptr, CORE_PRIORITYEVENT);
 
-  xTaskCreatePinnedToCore(keyScannerTask, "KeyScanner", STACK_KEYSCAN, &keyCfg,
-                          PRIORITY_KEYSCAN, nullptr, CORE_KEYSCAN);
+  xTaskCreatePinnedToCore(keyScannerTask, "KeyScanner", STACK_KEYSCAN,
+                          keyParams, PRIORITY_KEYSCAN, nullptr, CORE_KEYSCAN);
 }
 
 #endif
