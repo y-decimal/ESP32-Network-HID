78c6f86d6bc36a6a25c9fee54e1086cacc704b6f
y-decimal <83334510+y-decimal@users.noreply.github.com>
2025-12-28 18:58:48 +0100

    refactor: update keyScannerTask to use KeyScannerConfig for better configuration handling refactor: change keyScanner to be instantiated directly instead of using a pointer

--- src/tasks/KeyScannerTask.h
+++ src/tasks/KeyScannerTask.h
@@ -1,5 +1,6 @@
 #pragma once
 #include <modules/KeyScanner.h>
+#include <shared/ConfigTypes.h>
 #include <shared/EventTypes.h>
 #include <system/SystemConfig.h>
 
@@ -12,14 +13,18 @@ void keyEventCallback(uint16_t keyIndex, bool state) {
 }
 
 void keyScannerTask(void *arg) {
-  KeyScanner *keyScanner = static_cast<KeyScanner *>(arg);
+  KeyScannerConfig moduleCfg = *static_cast<KeyScannerConfig *>(arg);
 
-  keyScanner->registerOnKeyChangeCallback(keyEventCallback);
+  KeyScanner keyScanner = KeyScanner(moduleCfg.rowPins, moduleCfg.colPins,
+                                     moduleCfg.rows, moduleCfg.cols);
+
+  keyScanner.registerOnKeyChangeCallback(keyEventCallback);
 
   TickType_t previousWakeTime = xTaskGetTickCount();
+  TickType_t refreshRateToTicks = pdMS_TO_TICKS((moduleCfg.refreshRate / 1000));
 
   while (true) {
-    keyScanner->updateKeyState();
-    xTaskDelayUntil(&previousWakeTime, pdMS_TO_TICKS(PERIOD_KEYSCAN_MS));
+    keyScanner.updateKeyState();
+    xTaskDelayUntil(&previousWakeTime, pdMS_TO_TICKS(refreshRateToTicks));
   }
 }
