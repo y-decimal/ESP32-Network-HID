efb430b227f3a5dce02c05182b50e36cf5bb1cd5
y-decimal <83334510+y-decimal@users.noreply.github.com>
2025-12-28 17:43:20 +0100

    feat: implement keyPrintCallback for handling key events refactor: update setup function to register keyPrintCallback and improve initialization logging

--- src/main.cpp
+++ src/main.cpp
@@ -11,15 +11,25 @@ QueueHandle_t priorityEventQueue;
 QueueHandle_t eventQueue;
 KeyScanner keyScanner = KeyScanner(ROWPINS, COLPINS, rowCount, colCount);
 
+void keyPrintCallback(const Event &event) {
+  if (event.type != EventType::Key) {
+    printf("Received wrong event type\n");
+    return;
+  }
+  KeyEvent keyEvent = event.key;
+  uint8_t keyIndex = keyEvent.keyIndex;
+  bool state = keyEvent.state;
+  printf("Key event: Key %d %s\n", keyIndex, state ? "pressed" : "released");
+}
+
 void setup() {
   Serial.begin(115200);
+  delay(3000);
+  printf("initializing...\n");
+  EventRegistry::registerHandler(EventType::Key, keyPrintCallback);
   initSystemTasks();
+  printf("setup done\n");
 }
 
 void loop() {
-  KeyEvent event;
-  if (xQueueReceive(priorityEventQueue, &event, pdMS_TO_TICKS(100)) == pdPASS) {
-    printf("Key %d %s\n", event.keyIndex,
-           (event.state ? "pressed" : "released"));
-  }
 }
