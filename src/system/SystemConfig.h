fdc3c146b92b2c9ebb1a1599de5024586f14bdc1
y-decimal <83334510+y-decimal@users.noreply.github.com>
2025-12-29 17:33:35 +0100

    refactor: add BITMAPSIZE constant for key configuration

--- src/system/SystemConfig.h
+++ src/system/SystemConfig.h
@@ -4,6 +4,10 @@
 #include <stdint.h>
 #include <task.h>
 
+// Global constants
+static const uint8_t BITMAPSIZE =
+    6; // 6 Bytes for 127 bits, should be enough for every possible key
+
 // Priority Event Task Config
 static const uint32_t STACK_PRIORITYEVENT = 4096;
 static const UBaseType_t PRIORITY_PRIORITYEVENT = 4;
