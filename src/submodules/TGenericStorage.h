ea1cfd237bebfdcada633b6921f27e2209379c9b
y-decimal <83334510+y-decimal@users.noreply.github.com>
2025-12-30 00:54:47 +0100

    feat: add ThreadSafeGenericStorage wrapper for thread-safe data storage

---
+++ src/submodules/TGenericStorage.h
@@ -0,0 +1,45 @@
+#ifndef TGENERICSTORAGE_H
+#define TGENERICSTORAGE_H
+
+#include <submodules/GenericStorage.h>
+
+template <typename DATA> class ThreadSafeGenericStorage {
+private:
+  GenericStorage<DATA> storage;
+  mutable std::mutex mtx;
+
+public:
+  ThreadSafeGenericStorage(const char *key) : storage(key) {}
+
+  DATA get() const {
+    std::lock_guard<std::mutex> lock(mtx);
+    return storage.get();
+  }
+
+  void set(const DATA &in) {
+    std::lock_guard<std::mutex> lock(mtx);
+    storage.set(in);
+  }
+
+  bool isDirty() const {
+    std::lock_guard<std::mutex> lock(mtx);
+    return storage.isDirty();
+  }
+
+  void clearDirty() {
+    std::lock_guard<std::mutex> lock(mtx);
+    storage.clearDirty();
+  }
+
+  bool load() {
+    std::lock_guard<std::mutex> lock(mtx);
+    return storage.load();
+  }
+
+  bool save() {
+    std::lock_guard<std::mutex> lock(mtx);
+    return storage.save();
+  }
+};
+
+#endif
