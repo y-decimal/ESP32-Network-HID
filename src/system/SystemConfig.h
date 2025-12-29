57e782ca4ccab458ec69ee88bad246a5b595aaa4
y-decimal <83334510+y-decimal@users.noreply.github.com>
2025-12-29 20:25:29 +0100

    refactor: change static const to inline constexpr for task configuration constants

--- src/system/SystemConfig.h
+++ src/system/SystemConfig.h
@@ -10,18 +10,18 @@ inline constexpr uint8_t BITMAPSIZE =
     16; // 16 Bytes for 127 bits, should be enough for every possible key
 
 // Priority Event Task Config
-static const uint32_t STACK_PRIORITYEVENT = 4096;
-static const UBaseType_t PRIORITY_PRIORITYEVENT = 4;
-static const BaseType_t CORE_PRIORITYEVENT = 1;
+inline constexpr uint32_t STACK_PRIORITYEVENT = 4096;
+inline constexpr UBaseType_t PRIORITY_PRIORITYEVENT = 4;
+inline constexpr BaseType_t CORE_PRIORITYEVENT = 1;
 
 // KeyScanner Task Config
-static const uint32_t STACK_KEYSCAN = 4096;
-static const UBaseType_t PRIORITY_KEYSCAN = 5;
-static const BaseType_t CORE_KEYSCAN = 1;
+inline constexpr uint32_t STACK_KEYSCAN = 4096;
+inline constexpr UBaseType_t PRIORITY_KEYSCAN = 5;
+inline constexpr BaseType_t CORE_KEYSCAN = 1;
 
 // BitMapSender Task Config
-static const uint32_t STACK_BITMAP = 4096;
-static const UBaseType_t PRIORITY_BITMAP = 3;
-static const BaseType_t CORE_BITMAP = 0;
+inline constexpr uint32_t STACK_BITMAP = 4096;
+inline constexpr UBaseType_t PRIORITY_BITMAP = 3;
+inline constexpr BaseType_t CORE_BITMAP = 0;
 
 #endif
