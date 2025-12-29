974fe5a20b0a0042c79f7220dda7bb48706103e4
y-decimal <83334510+y-decimal@users.noreply.github.com>
2025-12-29 17:34:43 +0100

    feat: add BitMapEvent to EventTypes

--- src/shared/EventTypes.h
+++ src/shared/EventTypes.h
@@ -1,13 +1,18 @@
 #ifndef EVENTTYPES_H
 #define EVENTTYPES_H
-
 #include <stdint.h>
+#include <system/SystemConfig.h>
 
 enum class EventType : uint8_t { Key, ConfigUpdate, COUNT };
 
 struct KeyEvent {
   uint16_t keyIndex;
   bool state;
+};
+
+struct BitMapEvent {
+  uint8_t bitMapSize;
+  uint8_t bitMap[BITMAPSIZE];
 };
 
 struct Event {
