0d217981d126e762b65ca0d5190549e429d9cede
y-decimal <83334510+y-decimal@users.noreply.github.com>
2025-12-30 21:02:45 +0100

    refactor: move KeyScannerConfig implementation from ConfigTypes.h to KeyScannerConfig.h

---
+++ src/submodules/KeyScannerConfig.h
@@ -0,0 +1,76 @@
+#ifndef KEYSCANNERCONFIG_H
+#define KEYSCANNERCONFIG_H
+
+#include <interfaces/ISerializableStructs.h>
+#include <stdint.h>
+#include <vector>
+
+using pinType = std::vector<uint8_t>;
+using countType = uint8_t;
+
+class KeyScannerConfig {
+private:
+  countType rows = 0;
+  countType cols = 0;
+  uint8_t bitMapSize = 0;
+  pinType rowPins;
+  pinType colPins;
+  uint16_t refreshRate = 1;
+  uint16_t bitMapSendInterval = 5;
+
+  static constexpr uint16_t MIN_REFRESH_RATE = 1;
+  static constexpr uint16_t MAX_REFRESH_RATE = 4000;
+  static constexpr uint16_t MIN_BITMAP_INTERVAL = 2;
+  static constexpr uint16_t MAX_BITMAP_INTERVAL = 5000;
+
+public:
+  struct KeyCfgParams {
+    countType rows;
+    countType cols;
+    uint8_t *rowPins;
+    uint8_t *colPins;
+    uint16_t refreshRate;
+    uint16_t bitMapSendInterval;
+  };
+
+  void setPins(uint8_t *rowPinData, uint8_t rowSize, uint8_t *colPinData,
+               uint8_t colSize) {
+    rowPins.assign(rowPinData, rowPinData + rowSize);
+    rows = rowSize;
+    colPins.assign(colPinData, colPinData + colSize);
+    cols = colSize;
+    bitMapSize = (rows * cols + 7) / 8;
+  }
+
+  void setRefreshRate(uint16_t rate) {
+    if (rate < MIN_REFRESH_RATE || rate > MAX_REFRESH_RATE)
+      return;
+    refreshRate = rate;
+  }
+
+  void setBitMapSendInterval(uint16_t rateDivisor) {
+    if (MIN_BITMAP_INTERVAL < 2 || rateDivisor > MAX_BITMAP_INTERVAL)
+      // Limited to a range of 2-5000, to ensure bitmaps aren't sent every
+      // single loop (as this would most likely block the ESP communication),
+      // and 5000 to ensure we don't exceed the uint16_t limit and also stay
+      // within reasonable bounds for regular bitMap refreshes
+      return;
+    bitMapSendInterval = rateDivisor;
+  }
+
+  void setConfig(KeyCfgParams config) {
+    setPins(config.rowPins, config.rows, config.colPins, config.cols);
+    setRefreshRate(config.refreshRate);
+    setBitMapSendInterval(config.bitMapSendInterval);
+  }
+
+  pinType getRowPins() const { return rowPins; }
+  pinType getColPins() const { return colPins; }
+  countType getRowsCount() const { return rows; }
+  countType getColCount() const { return cols; }
+  uint8_t getBitmapSize() const { return bitMapSize; }
+  uint16_t getRefreshRate() const { return refreshRate; }
+  uint16_t getBitMapSendInterval() const { return bitMapSendInterval; }
+};
+
+#endif
