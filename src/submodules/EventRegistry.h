09d123edc052c0d7e444d6192aca47646d932b4a
y-decimal <83334510+y-decimal@users.noreply.github.com>
2025-12-31 00:45:13 +0100

    fix: change handler storage from single callback to vector for multiple callbacks per event type refactor: update function signatures in EventRegistry to reflect new handler storage

--- src/submodules/EventRegistry.h
+++ src/submodules/EventRegistry.h
@@ -3,15 +3,16 @@
 
 #include <cstring>
 #include <shared/EventTypes.h>
+#include <vector>
 
 class EventRegistry {
 public:
   using EventCallback = void (*)(const Event &);
   static void registerHandler(EventType type, EventCallback cb);
-  static EventCallback getHandler(EventType type);
+  static std::vector<EventCallback> getHandler(EventType type);
 
 private:
-  static EventCallback handlers[(size_t)EventType::COUNT];
+  static std::vector<EventCallback> handlers[(size_t)EventType::COUNT];
 };
 
 #endif
