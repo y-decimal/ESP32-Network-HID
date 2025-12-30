87ec1ceffb76830fbd80f03d9d1d8731579cbbd0
y-decimal <83334510+y-decimal@users.noreply.github.com>
2025-12-30 21:59:44 +0100

    feat: implement serialization methods in GlobalConfig class refactor: update GlobalConfig to inherit from Serializable

--- src/submodules/ConfigManager/GlobalConfig.cpp
+++ src/submodules/ConfigManager/GlobalConfig.cpp
@@ -19,3 +19,52 @@ void GlobalConfig::getMac(uint8_t *out, size_t size) {
     return;
   memcpy(out, deviceMac, sizeof(deviceMac));
 }
+
+size_t GlobalConfig::packSerialized(uint8_t *output, size_t size) const {
+  size_t ownSize = getSerializedSize();
+  if (size < ownSize)
+    return 0;
+  uint8_t buffer[ownSize] = {};
+
+  size_t index = 0;
+  size_t totalWrite = 0;
+  size_t objSize = 0;
+
+  objSize = sizeof(roles);
+  memcpy(buffer, roles, objSize);
+  index += objSize;
+  totalWrite += objSize;
+
+  objSize = sizeof(deviceMac);
+  memcpy(buffer + index, deviceMac, objSize);
+  totalWrite += objSize;
+
+  memcpy(output, buffer, totalWrite);
+
+  return totalWrite;
+}
+
+size_t GlobalConfig::unpackSerialized(const uint8_t *input, size_t size) {
+  size_t ownSize = getSerializedSize();
+  if (size > ownSize)
+    return 0;
+
+  size_t index = 0;
+  size_t totalWrite = 0;
+  size_t objSize = 0;
+
+  objSize = sizeof(roles);
+  memcpy(roles, input, objSize);
+  index += objSize;
+  totalWrite += objSize;
+
+  objSize = sizeof(deviceMac);
+  memcpy(deviceMac, input + index, objSize);
+  totalWrite += objSize;
+
+  return totalWrite;
+}
+
+size_t GlobalConfig::getSerializedSize() const {
+  return sizeof(roles) + sizeof(deviceMac);
+}
--- src/submodules/ConfigManager/GlobalConfig.h
+++ src/submodules/ConfigManager/GlobalConfig.h
@@ -2,9 +2,10 @@
 #define GLOBALCONFIG_H
 
 #include <cstring>
+#include <interfaces/ISerializableStructs.h>
 #include <shared/CommTypes.h>
 
-struct GlobalConfig {
+struct GlobalConfig : public Serializable {
 
   DeviceRole roles[(size_t)DeviceRole::Count]{};
   MacAddress deviceMac{};
@@ -16,6 +17,10 @@ struct GlobalConfig {
   void getRoles(DeviceRole *out, size_t size);
 
   void getMac(uint8_t *out, size_t size);
+
+  size_t packSerialized(uint8_t *output, size_t size) const override;
+  size_t unpackSerialized(const uint8_t *input, size_t size) override;
+  size_t getSerializedSize() const override;
 };
 
 #endif
