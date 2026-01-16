e58c6752fb97528f4327c75b5613b64c766e06e3
y-decimal <83334510+y-decimal@users.noreply.github.com>
2026-01-16 10:12:45 +0100

    docs: enhance documentation for Serializable interface and its methods

--- src/interfaces/ISerializableStructs.h
+++ src/interfaces/ISerializableStructs.h
@@ -4,12 +4,38 @@
 #include <cstring>
 #include <stdint.h>
 
+/**
+ * @brief Interface for serializable structures.
+ *
+ * The Serializable interface provides methods for packing and unpacking
+ * serialized data, as well as retrieving the size of the serialized data.
+ */
 class Serializable {
 public:
-  virtual ~Serializable() = default;
+  /**
+   * @brief Pack the structure into a serialized byte array.
+   * @param output Pointer to the output byte array.
+   * @param size Size of the output byte array.
+   * @return Number of bytes written to the output array.
+   */
   virtual size_t packSerialized(uint8_t *output, size_t size) const = 0;
+
+  /**
+   * @brief Unpack the structure from a serialized byte array.
+   * @param input Pointer to the input byte array.
+   * @param size Size of the input byte array.
+   * @return Number of bytes read from the input array.
+   */
   virtual size_t unpackSerialized(const uint8_t *input, size_t size) = 0;
+
+  /**
+   * @brief Get the size of the serialized structure.
+   * @return Size of the serialized structure in bytes.
+   */
   virtual size_t getSerializedSize() const = 0;
+
+  // Virtual destructor.
+  virtual ~Serializable() = default;
 };
 
 #endif
