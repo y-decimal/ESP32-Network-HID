fc898deff258648c75b4012c639ccfe5564e9bf8
y-decimal <83334510+y-decimal@users.noreply.github.com>
2026-01-16 10:11:05 +0100

    docs: enhance documentation for IGpio interface and its methods

--- src/interfaces/IGpio.h
+++ src/interfaces/IGpio.h
@@ -2,15 +2,46 @@
 #define IGPIO_H
 #include <cstdint>
 
-enum class PinMode : uint8_t { Input, InputPullup, Output };
-enum class PinState : uint8_t { Low = 0, High = 1 };
-
+/**
+ * @brief Interface for GPIO operations.
+ *
+ * The IGpio interface provides methods for configuring pin modes and
+ * reading/writing digital values to GPIO pins.
+ */
 class IGpio {
 public:
+  /**
+   * @brief Enumeration for GPIO pin modes.
+   */
+  enum class PinMode : uint8_t { Input, InputPullup, Output };
+
+  /**
+   * @brief Enumeration for GPIO pin states.
+   */
+  enum class PinState : uint8_t { Low = 0, High = 1 };
+
+  /**
+   * @brief Set the mode of a GPIO pin.
+   * @param pin The GPIO pin number.
+   * @param mode The mode to set for the pin (Input, InputPullup, Output).
+   */
   virtual void pinMode(uint8_t pin, PinMode mode) = 0;
+
+  /**
+   * @brief Write a digital value to a GPIO pin.
+   * @param pin The GPIO pin number.
+   * @param value The value to write to the pin (High or Low).
+   */
   virtual void digitalWrite(uint8_t pin, PinState value) = 0;
+
+  /**
+   * @brief Read a digital value from a GPIO pin.
+   * @param pin The GPIO pin number.
+   * @return The value read from the pin (High or Low).
+   */
   virtual PinState digitalRead(uint8_t pin) = 0;
 
+  // Virtual destructor
   virtual ~IGpio() = default;
 };
 #endif
