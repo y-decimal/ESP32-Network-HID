219011249e5b8323a698eb2f1476c5e8c86f9c0b
y-decimal <83334510+y-decimal@users.noreply.github.com>
2026-01-17 17:10:07 +0100

    feat: implement rudimentary role switching logic in initializeTasks()

--- src/system/TaskManager.cpp
+++ src/system/TaskManager.cpp
@@ -3,7 +3,8 @@
 TaskManager::TaskManager(ConfigManager &config, IGpio &gpio, IEspNow &espNow)
     : configManager(config), gpio(gpio), espNow(espNow) {}
 
-void TaskManager::start() {
+void TaskManager::start()
+{
 
   // Create queues and ensure they exist
   eventBusQueue = xQueueCreate(32, sizeof(Event));
@@ -17,11 +18,32 @@ void TaskManager::start() {
   configASSERT(taskCreated == pdPASS);
 }
 
-void TaskManager::initializeTasks() {
+void TaskManager::initializeTasks()
+{
 
   // Implement role based logic here later
   startEventBus();
   startKeyScanner(gpio);
-  startSlaveEspTask(espNow);
-  startMasterEspTask(espNow);
+
+  DeviceRole roles[static_cast<size_t>(DeviceRole::Count)] = {DeviceRole::Count};
+  GlobalConfig gCfg = configManager.getConfig<GlobalConfig>();
+  gCfg.getRoles(roles, static_cast<size_t>(DeviceRole::Count));
+  if (roles[0] == DeviceRole::Master)
+  {
+    printf("Starting Master ESP Task\n");
+    stopSlaveEspTask();
+    startMasterEspTask(espNow);
+  }
+  else if (roles[0] == DeviceRole::Keyboard)
+  {
+    printf("Starting Slave ESP Task\n");
+    stopMasterEspTask();
+    startSlaveEspTask(espNow);
+  }
+  else
+  {
+    printf("No ESP Task started\n");
+    stopMasterEspTask();
+    stopSlaveEspTask();
+  }
 }
