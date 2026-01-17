bca8057a306aa8a156577ebf80399088edb6c8d5
y-decimal <83334510+y-decimal@users.noreply.github.com>
2026-01-17 11:17:32 +0100

    refactor: improve class definition formatting for IEspNow interface refactor: update sendData method signature to include targetMac parameter

--- src/interfaces/IEspNow.h
+++ src/interfaces/IEspNow.h
@@ -4,14 +4,13 @@
 #include <functional>
 #include <stdint.h>
 
-class IEspNow {
+class IEspNow
+{
 public:
-  using receiveCallback =
-      std::function<void(uint8_t *data, size_t length, uint8_t senderMac[6])>;
+    using receiveCallback = std::function<void(uint8_t *data, size_t length, uint8_t senderMac[6])>;
 
-  virtual bool sendData(uint8_t packetType, uint8_t *data, size_t length) = 0;
-  virtual bool registerPacketTypeCallback(uint8_t packetType,
-                                          receiveCallback callback) = 0;
+    virtual bool sendData(uint8_t packetType, uint8_t *data, size_t length, uint8_t targetMac[6]) = 0;
+    virtual bool registerPacketTypeCallback(uint8_t packetType, receiveCallback callback) = 0;
 };
 
 #endif
