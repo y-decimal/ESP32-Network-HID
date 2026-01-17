8329360c2c739c23dbc8a93ca919b4f6dcce40fd
y-decimal <83334510+y-decimal@users.noreply.github.com>
2026-01-17 10:21:47 +0100

    style: format receiveCallback definition for better readability

--- src/interfaces/IEspNow.h
+++ src/interfaces/IEspNow.h
@@ -1,11 +1,13 @@
 #ifndef IESP_NOW_H
 #define IESP_NOW_H
 
+#include <functional>
 #include <stdint.h>
 
 class IEspNow {
 public:
-  using receiveCallback = std::function<void(uint8_t *data, size_t length, uint8_t senderMac[6])>;
+  using receiveCallback =
+      std::function<void(uint8_t *data, size_t length, uint8_t senderMac[6])>;
 
   virtual bool sendData(uint8_t packetType, uint8_t *data, size_t length) = 0;
   virtual bool registerPacketTypeCallback(uint8_t packetType,
