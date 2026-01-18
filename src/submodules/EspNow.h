a7216f3a144975f9e403e738586385348e9d1f5f
y-decimal <83334510+y-decimal@users.noreply.github.com>
2026-01-18 12:41:04 +0100

    fix: initialize receiveCallback array with nullptr

--- src/submodules/EspNow.h
+++ src/submodules/EspNow.h
@@ -15,7 +15,7 @@ public:
     bool clearCallback(uint8_t packetType) override;
 
 private:
-    receiveCallback callbacks[256];
+    receiveCallback callbacks[256] = {nullptr};
     bool initialized = false;
     static EspNow *instance;
 
