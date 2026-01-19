ffbdb4b2c7b6002e75c3f8f143980a1b56f6607d
y-decimal <83334510+y-decimal@users.noreply.github.com>
2026-01-19 09:06:17 +0100

    feat: implement ILogSink interface for logging functionality feat: add Logger class with support for log levels and global sink management

---
+++ src/submodules/Logger.cpp
@@ -0,0 +1,140 @@
+#include <submodules/Logger.h>
+#include <mutex>
+namespace
+{
+    struct LoggerCore
+    {
+        static std::mutex mutex;
+        static ILogSink *globalSink;
+        static std::unordered_map<std::string, Logger::LogLevel> namespaceLevels;
+        static Logger::globalLogCallback globalCallback;
+        static Logger::LogLevel defaultLogLevel;
+    };
+
+    std::mutex LoggerCore::mutex{};
+    ILogSink *LoggerCore::globalSink = nullptr;
+    std::unordered_map<std::string, Logger::LogLevel> LoggerCore::namespaceLevels{};
+    Logger::globalLogCallback LoggerCore::globalCallback = nullptr;
+    Logger::LogLevel LoggerCore::defaultLogLevel = Logger::LogLevel::info;
+}
+
+static const char *logLevelToString(Logger::LogLevel level)
+{
+    switch (level)
+    {
+    case Logger::LogLevel::disabled:
+        return "DISABLED";
+    case Logger::LogLevel::error:
+        return "ERROR";
+    case Logger::LogLevel::warn:
+        return "WARN";
+    case Logger::LogLevel::info:
+        return "INFO";
+    case Logger::LogLevel::debug:
+        return "DEBUG";
+    default:
+        return "UNKNOWN";
+    }
+}
+
+Logger::Logger(const char *logNamespace)
+{
+    this->logNamespace.assign(logNamespace);
+}
+
+void Logger::setGlobalSink(ILogSink *globalSink)
+{
+    std::lock_guard<std::mutex> lock(LoggerCore::mutex);
+    LoggerCore::globalSink = globalSink;
+}
+
+void Logger::setNamespaceLevel(const char *logNamespace, LogLevel level)
+{
+    std::lock_guard<std::mutex> lock(LoggerCore::mutex);
+    LoggerCore::namespaceLevels[logNamespace] = level;
+}
+
+Logger::LogLevel Logger::getNamespaceLevel(const char *logNamespace)
+{
+    std::lock_guard<std::mutex> lock(LoggerCore::mutex);
+    auto it = LoggerCore::namespaceLevels.find(logNamespace);
+    if (it == LoggerCore::namespaceLevels.end())
+        return LoggerCore::defaultLogLevel;
+    return it->second;
+}
+
+void Logger::setDefaultLogLevel(LogLevel level)
+{
+    std::lock_guard<std::mutex> lock(LoggerCore::mutex);
+    LoggerCore::defaultLogLevel = level;
+}
+
+void Logger::setLogCallback(globalLogCallback callback)
+{
+    std::lock_guard<std::mutex> lock(LoggerCore::mutex);
+    LoggerCore::globalCallback = std::move(callback);
+}
+
+void Logger::writeWithNamespace(const char *logNamespace, LogLevel level, const char *message)
+{
+    std::string formatted = std::string("(") + logLevelToString(level) + ") " + " : " + message;
+    const char *result = formatted.c_str(); // Only valid while formatted is in scope
+
+    LoggerCore::globalSink->writeLog(logNamespace, result);
+}
+
+void Logger::setMode(LogMode mode)
+{
+    this->mode = mode;
+}
+
+void Logger::error(const char *message)
+{
+    const char *logNs = this->logNamespace.c_str();
+
+    if (getNamespaceLevel(logNs) != LogLevel::error)
+        return;
+
+    log(logNs, LogLevel::error, message);
+}
+
+void Logger::warn(const char *message)
+{
+    const char *logNs = this->logNamespace.c_str();
+
+    if (getNamespaceLevel(logNs) != LogLevel::warn)
+        return;
+
+    log(logNs, LogLevel::warn, message);
+}
+
+void Logger::info(const char *message)
+{
+    const char *logNs = this->logNamespace.c_str();
+
+    if (getNamespaceLevel(logNs) != LogLevel::info)
+        return;
+
+    log(logNs, LogLevel::info, message);
+}
+
+void Logger::debug(const char *message)
+{
+    const char *logNs = this->logNamespace.c_str();
+
+    if (getNamespaceLevel(logNs) != LogLevel::debug)
+        return;
+
+    log(logNs, LogLevel::debug, message);
+}
+
+void Logger::log(const char *logNamespace, LogLevel level, const char *message)
+{
+    switch (mode)
+    {
+    case LogMode::Local:
+        writeWithNamespace(logNamespace, LogLevel::error, message);
+    case LogMode::Global:
+        LoggerCore::globalCallback(logNamespace, LogLevel::error, message);
+    }
+}
