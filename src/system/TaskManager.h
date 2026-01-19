89bcbd0c4c8323f77a553dabf494aaa0ee2543f8
y-decimal <83334510+y-decimal@users.noreply.github.com>
2026-01-19 19:08:18 +0100

    refactor: restructure TaskManager class to use Platform struct for dependencies refactor: initialize task classes with platform parameters in TaskManager constructor refactor: enhance start method in TaskManager to handle device roles and task starting

--- src/system/TaskManager.h
+++ src/system/TaskManager.h
@@ -2,22 +2,81 @@
 #define TASKMANAGER_H
 
 #include <FreeRTOS.h>
+
 #include <modules/EventBusTask.h>
+#include <modules/LoggerTask.h>
+#include <modules/KeyScannerTask.h>
+#include <modules/MasterTask.h>
+#include <modules/SlaveTask.h>
+
+#include <submodules/ConfigManager/ConfigManager.h>
+#include <submodules/Logger.h>
 #include <system/SystemConfig.h>
+
+#include <interfaces/IGpio.h>
+#include <interfaces/ITransport.h>
+#include <interfaces/ILogSink.h>
+#include <interfaces/IStorage.h>
 
 class TaskManager
 {
 public:
-  TaskManager() : eventBusTask(eventBusQueue) {}; // Task classes are initialized here
+  struct Platform
+  {
+    IGpio &gpio;
+    ITransport &transport;
+    ILogSink &logSink;
+    IStorage &storage;
+  };
 
-  void start() {
+  TaskManager(Platform &platform)
+      : platform(platform),
+        configManager(&platform.storage),
+        eventBusTask(),
+        loggerTask(platform.logSink),
+        keyScannerTask(configManager, platform.gpio),
+        masterTask(platform.transport),
+        slaveTask(platform.transport)
+  {
+  }
+
+  void start()
+  {
+    loggerTask.start({STACK_LOGGER, PRIORITY_LOGGER, CORE_LOGGER});
     eventBusTask.start({STACK_EVENTBUS, PRIORITY_EVENTBUS, CORE_EVENTBUS});
+
+    bool configLoaded = configManager.loadConfig();
+    if (!configLoaded)
+      mainLog.warn("Failed to load configuration, using defaults");
+    DeviceRole roles[(size_t)DeviceRole::Count];
+    configManager.getConfig<GlobalConfig>().getRoles(roles, (size_t)DeviceRole::Count);
+    for (size_t i = 0; i < (size_t)DeviceRole::Count; i++)
+    {
+      mainLog.info("Role %d: %d", i, static_cast<uint8_t>(roles[i]));
+    }
+    if (roles[0] == DeviceRole::Master)
+    {
+      masterTask.start({STACK_MASTER, PRIORITY_MASTER, CORE_MASTER});
+    }
+    if (roles[0] == DeviceRole::Keyboard)
+    {
+      slaveTask.start({STACK_SLAVE, PRIORITY_SLAVE, CORE_SLAVE});
+      keyScannerTask.start({STACK_KEYSCAN, PRIORITY_KEYSCAN, CORE_KEYSCAN});
+    }
   }
 
 private:
+  Platform &platform;
+  ConfigManager configManager;
+
+  Logger mainLog = Logger("TaskManager");
+
   // Task classes are stored here
-  QueueHandle_t eventBusQueue = xQueueCreate(32, sizeof(Event));
   EventBusTask eventBusTask;
+  LoggerTask loggerTask;
+  KeyScannerTask keyScannerTask;
+  MasterTask masterTask;
+  SlaveTask slaveTask;
 };
 
 #endif
