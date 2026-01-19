ee80eb405cecb8f57de2bf5c35fc95d285d69dd4
y-decimal <83334510+y-decimal@users.noreply.github.com>
2026-01-19 15:41:10 +0100

    feat: add LoggerTask class for logging functionality

---
+++ src/modules/LoggerTask.h
@@ -0,0 +1,28 @@
+#ifndef LOGGERTASK_H
+#define LOGGERTASK_H
+
+#include <interfaces/ITask.h>
+#include <queue.h>
+#include <submodules/Logger.h>
+
+class LoggerTask : public ITask
+{
+public:
+    LoggerTask(ILogSink &logSink);
+    ~LoggerTask();
+    void start(TaskParameters params) override;
+    void stop() override;
+    void restart(TaskParameters params) override;
+
+private:
+    QueueHandle_t localQueue;
+    TaskHandle_t loggerHandle = nullptr;
+    static LoggerTask* instance;
+    ILogSink &logSink;
+    
+    static void taskEntry(void *param);
+
+    static void callback(const char *logNamespace, Logger::LogLevel level, const char *message);
+};
+
+#endif
