e44ab6f10e1a7c705c75b45d3d838f477382ba56
y-decimal <83334510+y-decimal@users.noreply.github.com>
2026-01-19 18:57:37 +0100

    feat: add KeyScannerTask class for key scanning functionality

---
+++ src/modules/KeyScannerTask.h
@@ -0,0 +1,34 @@
+#ifndef KEYSCANNERTASK_H
+#define KEYSCANNERTASK_H
+
+#include <interfaces/ITask.h>
+#include <submodules/KeyScanner.h>
+#include <submodules/ConfigManager/ConfigManager.h>
+#include <submodules/Logger.h>
+#include <submodules/EventRegistry.h>
+#include <queue.h>
+
+static constexpr const char *KEYSCANNER_NAMESPACE = "KeyScannerTask";
+
+class KeyScannerTask : public ITask
+{
+public:
+    KeyScannerTask(ConfigManager &configManager, IGpio &gpio);
+    ~KeyScannerTask();
+    void start(TaskParameters params) override;
+    void stop() override;
+    void restart(TaskParameters params) override;
+
+private:
+    ConfigManager *configManagerRef = nullptr;
+    IGpio *gpioRef = nullptr;
+    TaskHandle_t keyScannerTaskHandle = nullptr;
+    Logger *internalLog = nullptr;
+    static KeyScannerTask *instance;
+
+    static void taskEntry(void *param);
+    static void keyEventCallback(uint16_t keyIndex, bool state);
+    static void sendBitMapEvent(uint8_t bitMapSize, uint8_t *bitMap);
+};
+
+#endif
