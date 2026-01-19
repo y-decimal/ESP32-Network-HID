8ab08f7656b07bd6edd681724899085df6e719cf
y-decimal <83334510+y-decimal@users.noreply.github.com>
2026-01-19 15:26:53 +0100

    refactor: implement changes to header structure refactor: update taskEntry to use localQueue instead of queue parameter

--- src/modules/EventBusTask.cpp
+++ src/modules/EventBusTask.cpp
@@ -1,12 +1,35 @@
 #include <modules/EventBusTask.h>
 
+EventBusTask::EventBusTask()
+{
+  if (instance != nullptr)
+    return;
+  instance = this;
+  localQueue = xQueueCreate(32, sizeof(Event));
+  EventRegistry::registerPushCallback(staticPushCallback);
+}
 
-void EventBusTask::taskEntry(void *param) {
+EventBusTask::~EventBusTask()
+{
+  if (localQueue != nullptr)
+  {
+    vQueueDelete(localQueue);
+    localQueue = nullptr;
+  }
+  EventRegistry::clearPushCallback();
+  instance = nullptr;
+}
+
+// Main task loop
+void EventBusTask::taskEntry(void *param)
+{
   QueueHandle_t queue = static_cast<QueueHandle_t>(param);
   Event event;
 
-  while (true) {
-    if (xQueueReceive(queue, &event, portMAX_DELAY)) {
+  while (true)
+  {
+    if (xQueueReceive(queue, &event, portMAX_DELAY))
+    {
       const auto handlers = EventRegistry::getHandler(event.type);
       for (auto callback : handlers)
         callback(event);
@@ -14,30 +37,54 @@ void EventBusTask::taskEntry(void *param) {
   }
 }
 
-// EventHandler helper functions
+// Event pushing helper functions
 
-void EventBusTask::start(TaskParameters params) {
+bool EventBusTask::staticPushCallback(const Event &event)
+{
+  if (instance != nullptr)
+  {
+    return instance->pushToQueue(event);
+  }
+  return false;
+}
+
+bool EventBusTask::pushToQueue(const Event &event)
+{
+  if (localQueue != nullptr)
+  {
+    return xQueueSend(localQueue, &event, 0) == pdTRUE;
+  }
+  return false;
+}
+
+// Task lifecycle methods
+
+void EventBusTask::start(TaskParameters params)
+{
 
   if (eventBusHandle != nullptr)
     return;
 
   BaseType_t result = xTaskCreatePinnedToCore(
-      EventBusTask::taskEntry, "EventBusHandler", params.stackSize, queue,
+      EventBusTask::taskEntry, "EventBusHandler", params.stackSize, localQueue,
       params.priority, &eventBusHandle, params.coreAffinity);
 
-  if (result != pdPASS) {
+  if (result != pdPASS)
+  {
     eventBusHandle = nullptr;
   }
 }
 
-void EventBusTask::stop() {
+void EventBusTask::stop()
+{
   if (eventBusHandle == nullptr)
     return;
   vTaskDelete(eventBusHandle);
   eventBusHandle = nullptr;
 }
 
-void EventBusTask::restart(TaskParameters params) {
+void EventBusTask::restart(TaskParameters params)
+{
   if (eventBusHandle != nullptr)
     stop();
   start(params);
