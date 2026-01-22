cf3c0783d3cda37912c3bcafe45b603b8026ca1b
y-decimal <83334510+y-decimal@users.noreply.github.com>
2026-01-22 12:14:44 +0100

    refactor: update method signatures for clarity and consistency refactor: rename mapToHidBitmap to mapBitmapToHidBitmap for better readability

--- src/submodules/HidMapper.h
+++ src/submodules/HidMapper.h
@@ -14,19 +14,20 @@ private:
   uint8_t hidBitmap[32]{0}; // 32 Bytes = 256 bits for HID report
 
   std::unordered_map<uint8_t, std::vector<uint8_t>> localToHidMaps;
-  
+
   void setBit(uint8_t bitmapBitIndex);
   void clearBit(uint8_t bitmapBitIndex);
+  void updateHidBit(bool bitState, uint8_t bitmapBitIndex);
 
 public:
   HidMapper();
 
   void insertMap(const uint8_t *map, size_t mapSize, uint8_t mapId);
 
-  void mapToHidBitmap(const uint8_t *bitmap, size_t bitmapSize, uint8_t mapId);
+  void mapBitmapToHidBitmap(const uint8_t *bitmap, size_t bitmapSize, uint8_t mapId);
+  void mapIndexToHidBitmap(uint8_t index, bool bitState, uint8_t mapId);
 
-  void updateHidBit(bool bitState, uint8_t bitmapBitIndex);
-
+  size_t getBitmapSize() { return 32; }
   size_t copyBitmap(uint8_t *dest, size_t destSize) const;
 };
 
