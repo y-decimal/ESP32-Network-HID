72a5a680d70ffbc834fb903a575cfd02d798adf7
y-decimal <83334510+y-decimal@users.noreply.github.com>
2026-01-22 10:13:47 +0100

    refactor: update HidMapper class structure and member variables refactor: change hidBitmap size to 32 bytes for expanded HID report capability feat: add local map storage and update public API to reflect change

--- src/submodules/HidMapper.h
+++ src/submodules/HidMapper.h
@@ -4,23 +4,27 @@
 #include <cstdint>
 #include <cstring>
 #include <stdio.h>
+#include <vector>
+#include <unordered_map>
 
-class HidMapper {
+class HidMapper
+{
 
 private:
-  uint8_t hidBitmap[16]; // 16 Bytes = 128 bits for HID report
-                         // Enough for our use case
+  uint8_t hidBitmap[32]{0}; // 32 Bytes = 256 bits for HID report
+
+  std::unordered_map<uint8_t, std::vector<uint8_t>> localToHidMaps;
 
 public:
-  enum class KROMode { KRO_6 = 6, KRO_12 = 12, NKRO = 128 };
-
   HidMapper();
 
-  void handleBitmapEvent(const uint8_t *bitmap, size_t bitmapSize, uint8_t *map,
-                         size_t mapSize);
+  void insertMap(const uint8_t *map, size_t mapSize, uint8_t mapId);
 
-  size_t generateHidReport(uint8_t *out, size_t outSize,
-                           KROMode mode = KROMode::KRO_6);
+  void mapToHidBitmap(const uint8_t *bitmap, size_t bitmapSize, uint8_t mapId);
+
+  void updateHidBit(bool bitState, uint8_t bitmapPos);
+
+  size_t copyBitmap(uint8_t *dest, size_t destSize) const;
 };
 
 #endif
