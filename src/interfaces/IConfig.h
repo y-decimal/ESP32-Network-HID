f0368915309c7b0333811805c4967a82482848d6
y-decimal <83334510+y-decimal@users.noreply.github.com>
2026-01-23 09:28:15 +0100

    refactor: update IConfig to include setStorage method

--- src/interfaces/IConfig.h
+++ src/interfaces/IConfig.h
@@ -1,13 +1,16 @@
 #ifndef ICONFIG_H
 #define ICONFIG_H
 
-#include <interfaces/ISerializableStructs.h>
+#include <interfaces/ISerializable.h>
 #include <interfaces/IStorage.h>
 
 class IConfig : public ISerializable
 {
 public:
     virtual ~IConfig() = default;
+
+    virtual void setStorage(IStorage storage) = 0;
+
     virtual bool save() = 0;
     virtual bool load() = 0;
 };
