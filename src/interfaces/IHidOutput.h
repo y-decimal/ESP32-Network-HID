704f01fd469f0d472a9c7a71b6d09542581fb131
y-decimal <83334510+y-decimal@users.noreply.github.com>
2026-01-26 20:26:55 +0100

    refactor: add COUNT to KroMode enum for clarity refactor: reorder methods in IHidOutput interface for consistency

--- src/interfaces/IHidOutput.h
+++ src/interfaces/IHidOutput.h
@@ -11,10 +11,14 @@ public:
     enum class KroMode : uint8_t
     {
         MODE_6KRO,
-        MODE_NKRO
+        MODE_NKRO,
+        COUNT
     };
 
-    virtual void sendHidReport(uint8_t *hidBitmap, size_t size) = 0;
+    static constexpr const uint8_t getKroModeValue(KroMode mode) { return static_cast<uint8_t>(mode); }
+
+    virtual bool initialize() = 0;
+    virtual void sendHidReport(const uint8_t *hidBitmap, size_t size) = 0;
     virtual KroMode getKroMode() = 0;
 };
 
