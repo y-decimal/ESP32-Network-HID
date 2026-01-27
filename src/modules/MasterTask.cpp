057c98fbd9f921402c8300b2d969da066605d32e
y-decimal <83334510+y-decimal@users.noreply.github.com>
2026-01-27 02:53:02 +0100

    fix: fix double free issue, transport protocol and internal event handling now free their own allocated data after triggering callback

--- src/modules/MasterTask.cpp
+++ src/modules/MasterTask.cpp
@@ -158,7 +158,6 @@ void MasterTask::bitmapReceiveCallback(RawBitmapEvent &bitmapEvent, uint8_t send
   }
 
   instance->hidMapper.mapBitmapToHidBitmap(bitmapEvent.bitMapData, bitmapEvent.bitmapSize, senderId);
-  free(bitmapEvent.bitMapData);
   log.debug("Pushed bitmap event from device ID %u to HidMapper", senderId);
 
   std::vector<uint8_t> currentBitmap{0};
